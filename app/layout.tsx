62ec30c14bd54e9a5195141cf7648443cb058510
nitin00201 <kabinitin2002@gmail.com>
2024-11-17 14:56:22 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { shadesOfPurple } from "@clerk/themes";
+
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ZOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          baseTheme: shadesOfPurple,
+
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorPrimary: "#3b82f6",
+            colorBackground: "#1a202c",
+            colorInputBackground: "#2D3748",
+            colorInputText: "#F3F4F6",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
